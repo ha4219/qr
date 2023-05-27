a252d0bd0c4ad54671b3778ca1d62ea61cf3dad0
ha4219 <ha4219@naver.com>
2023-05-27 17:41:16 +0900

    handle error

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -5,17 +5,25 @@ import QRCode from "qrcode";
 type Data = {
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { value } = req.query;
+  const { value, size } = req.query;
   res.setHeader('Content-Type', 'image/png');
   if (!value) {
     res.status(400).send('value is required!');
   }
-  QRCode.toBuffer(value as string, (err, data) => {
-    res.statusCode = 200;
-    res.end(data);
-  });
+  let s = Number(size) || 150;
+  try {
+    const result = await QRCode.toBuffer(value as string, {
+      width: s,
+      type: 'png',
+    });
+    res.status(200).end(result);
+  
+  }
+  catch(e) {
+    res.status(500).send('Internel error');
+  }
 }
